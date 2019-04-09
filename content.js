055da79545bcf6d359c6b04776640fa14d6e90a6
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-09 20:38:18 +0200

    get salary info

--- content.js
+++ content.js
@@ -1,7 +1,14 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
-const url = 'https://nofluffjobs.com//job/machine-learning-engineer-bytedance-tiktok-u1rwecbs';
+const url = 'https://nofluffjobs.com/job/node-js-developer-astek-polska-fa3oeqn7?criteria=category%253Dbackend';
 const fs = require('fs');
+
+jobInfo = {
+    salaryMin: 0,
+    salaryMax: 0,
+    salaryCurrency: ' ',
+    salaryInfo: '',
+};
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -15,17 +22,41 @@ const fs = require('fs');
     const position = $('.posting-header-description h1').text();
 
     let info = []
-    const other = $('.posting-header-description dd').each(function(i, elem) {
+    $('.posting-header-description dd').each(function(i, elem) {
         // console.log($(this).text());
         info[i] = $(this).text();
     });
 
+    const salary = $('.posting-main-info h4').text().split(' ');
+    // remove - sign from array
+    for(var i = 0; i < salary.length; i++) {
+        if(salary[i] === '-') {
+            salary.splice(i, 1);
+        }
+    }
+    jobInfo.salaryMin = salary[0];
+    jobInfo.salaryMax = salary[1];
+    jobInfo.salaryCurrency = salary[2];
+    jobInfo.salaryInfo = $('.posting-main-info p').text();
+    console.log(jobInfo);
+
     const companyName = info[0];
     const companySize = info[1];
-    const companyLocation = info[2];
+    const companyLocation = (info[2]).split(', ');
+    const companyLocationCity = companyLocation[0];
+    const companyLocationCountry = companyLocation[1];
     const whenToStart = info[3];
-    const typeOfContract = info[4];
-    const seniorityLevel = info[5];
+    const typeOfContract = (info[4]).trim();
+    const seniorityLevel = (info[5]).split(', ');
+
+    console.log(companyName);
+    console.log(companySize);
+    console.log(companyLocation);
+    console.log(companyLocationCountry);
+    console.log(companyLocationCity);
+    console.log(whenToStart);
+    console.log(typeOfContract);
+    console.log(seniorityLevel);
 
     await browser.close();
 })();   
