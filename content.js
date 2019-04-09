07c104159fbbf27269cead27ca1d221e348bd64a
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-09 21:12:19 +0200

    added requirements

--- content.js
+++ content.js
@@ -1,13 +1,25 @@
+// TODO: double salary info is not working
+
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
-const url = 'https://nofluffjobs.com/job/node-js-developer-astek-polska-fa3oeqn7?criteria=category%253Dbackend';
+const url = 'https://nofluffjobs.com/job/java-developer-4-years-of-experience-prodata-consult-ybkpfh9u?criteria=category%253Dbackend';
 const fs = require('fs');
 
 jobInfo = {
+    position: '',
     salaryMin: 0,
     salaryMax: 0,
     salaryCurrency: ' ',
-    salaryInfo: '',
+    salaryRate: 0,
+    contractType: '',
+    typeOfContract: '',
+    seniorityLevel: [],
+    whenToStart: '',
+    companyName: '',
+    companySize: 0,
+    companyLocationCity: '',
+    companyLocationCountry: '',
+    requirements: [],
 };
 
 (async () => {
@@ -19,13 +31,22 @@ jobInfo = {
     var $ = cheerio.load(content, {"waitUntil" : "networkidle0"});
 
     // get all links
-    const position = $('.posting-header-description h1').text();
+    jobInfo.position = $('.posting-header-description h1').text();
 
     let info = []
     $('.posting-header-description dd').each(function(i, elem) {
         // console.log($(this).text());
         info[i] = $(this).text();
     });
+
+    jobInfo.companyName = info[0];
+    jobInfo.companySize = info[1];
+    const companyLocation = (info[2]).split(', ');
+    jobInfo.companyLocationCity = companyLocation[0];
+    jobInfo.companyLocationCountry = (companyLocation[1]).trim();
+    jobInfo.whenToStart = info[3];
+    jobInfo.typeOfContract = (info[4]).trim();
+    jobInfo.seniorityLevel = (info[5]).split(', ');
 
     const salary = $('.posting-main-info h4').text().split(' ');
     // remove - sign from array
@@ -37,26 +58,35 @@ jobInfo = {
     jobInfo.salaryMin = salary[0];
     jobInfo.salaryMax = salary[1];
     jobInfo.salaryCurrency = salary[2];
-    jobInfo.salaryInfo = $('.posting-main-info p').text();
+
+    const salaryInfoText = $('.posting-main-info p').text();
+    // check contract type
+    if(salaryInfoText.toUpperCase().includes('B2B')) {
+        jobInfo.contractType = 'B2B';
+    } 
+    else if(salaryInfoText.toUpperCase().includes('UOP')) {
+        jobInfo.contractType = 'UoP';
+    }
+
+    // check payment rate
+    if(salaryInfoText.toUpperCase().includes('MONTH')) {
+        jobInfo.salaryRate = 'month';
+    }
+    else if(salaryInfoText.toUpperCase().includes('DAY')) {
+        jobInfo.salaryRate = 'day';
+    }
+
+    $('.requirement').each(function(i, elem) {
+        jobInfo.requirements[i] = ($(this).text());
+    });
+
+
+    // const requirementsGray = $('.requirement requirement-gray').each(function(i, elem) {
+    //     console.log($(this).text());
+    // });
+
+
     console.log(jobInfo);
-
-    const companyName = info[0];
-    const companySize = info[1];
-    const companyLocation = (info[2]).split(', ');
-    const companyLocationCity = companyLocation[0];
-    const companyLocationCountry = companyLocation[1];
-    const whenToStart = info[3];
-    const typeOfContract = (info[4]).trim();
-    const seniorityLevel = (info[5]).split(', ');
-
-    console.log(companyName);
-    console.log(companySize);
-    console.log(companyLocation);
-    console.log(companyLocationCountry);
-    console.log(companyLocationCity);
-    console.log(whenToStart);
-    console.log(typeOfContract);
-    console.log(seniorityLevel);
 
     await browser.close();
 })();   
