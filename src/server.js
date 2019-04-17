95e633a43c515e0a61eb718cce45a46e97c5d333
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-17 18:08:44 +0200

    added links to other jobs categories

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-// parse link only if link not in db
+// TODO: check other categories like frontend, devops, etc.
 
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
@@ -7,7 +7,22 @@ const db = require('./db');
 import jobParser from './content';
 import { sleep } from './common'
 
-const url = 'https://nofluffjobs.com/jobs/backend/';
+const url = [
+    'https://nofluffjobs.com/jobs/backend/',
+    'https://nofluffjobs.com/jobs/fullstack',
+    'https://nofluffjobs.com/jobs/mobile',
+    'https://nofluffjobs.com/jobs/frontend',
+    'https://nofluffjobs.com/jobs/testing',
+    'https://nofluffjobs.com/jobs/devops',
+    'https://nofluffjobs.com/jobs/hr',
+    'https://nofluffjobs.com/jobs/trainee',
+    'https://nofluffjobs.com/jobs/ux',
+    'https://nofluffjobs.com/jobs/support',
+    'https://nofluffjobs.com/jobs/project-manager',
+    'https://nofluffjobs.com/jobs/business-analyst',
+    'https://nofluffjobs.com/jobs/other'
+];
+
 var jobLinks = [];
 
 // db related
@@ -33,11 +48,10 @@ db.connect((err) => {
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url[0]);
     await page.waitFor(3000);
     let content = await page.content();
     
-    console.log('Getting list of all links');
     var $ = cheerio.load(content);
 
     // get all links
@@ -51,7 +65,7 @@ db.connect((err) => {
 
     // check if parsed any links
     if(jobLinks.length != 0) {
-        for(let i = 5; i < 15; i++) {
+        for(let i = 0; i < 3; i++) {
             const timeCounter = Math.floor((Math.random() * 30000) + 2000);
     
             // check if link exists in db
