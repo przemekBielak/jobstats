a63cf5460f5518cc97ba7c34d6179da4d18fec77
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-17 23:10:15 +0200

    added express backend to send json of mongogb element

---
+++ src/expressTest.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./db');
+
+const collection = "jobs";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api', (req, res) => {
+    db.getDB().collection(collection).findOne({"_id":"https://nofluffjobs.com/job/ambitious-recruiter-itds-polska-tv7yntlx?criteria=category%253Dhr"}, (err, documents) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(documents);
+            res.json(documents);
+        }
+    });
+});
+
+// Program start here
+db.connect((err) => {
+    if(err) {
+        console.log('Unable to connect to database');
+        process.exit(1);
+    } else {
+        app.listen(3000, () => {
+            console.log('Connected to database, listening on port 3000');
+        })
+    }
+});
+
