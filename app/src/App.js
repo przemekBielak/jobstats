9c3308236b9e65e3ff4a65e7eaa1029cf47f8f52
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-18 20:06:43 +0200

    added button connecting backend with react

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: 'text'
+  };
+
+  get = () => {
+    fetch('http://localhost:8080/api')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({text: data[0].companyName});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <button onClick={this.get}>
+          click here
+        </button>
+
+        <h1>{this.state.text}</h1>
+        
       </div>
     );
   }
