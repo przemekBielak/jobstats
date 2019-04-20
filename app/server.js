8b25a04eaacd22712adc91f7d971c1005bbfe485
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-04-20 21:30:38 +0200

    server side refactoring

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./db');
+const MongoClient = require('mongodb').MongoClient;
 
 const APP_PORT = 8080;
 
+const mongoUrl = "mongodb://localhost:27017/";
+const mongoOptions = { useNewUrlParser: true };
+const dbname =  "jobs";
 const collection = "jobs";
+
+var db = null;
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,12 +20,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+
 app.post('/lang-count/', (req, res) => {
     var lang = req.body.lang;
     var city = req.body.city;
 
     if(city != "Any") {
-        db.getDB().collection(collection).countDocuments(
+        db.countDocuments(
             {
                 $and:
                 [
@@ -29,36 +35,32 @@ app.post('/lang-count/', (req, res) => {
                 ]
             }
         )
-        .then((count) => {
-            res.json({count: count});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        .then((count) => res.json({count: count}))
+        .catch((err) => console.log(err));
     }
     else {
-        db.getDB().collection(collection).countDocuments(
+        db.countDocuments(
             {"requirementsMustHave":lang}
         )
-        .then((count) => {
-            res.json({count: count});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        .then((count) => res.json({count: count}))
+        .catch((err) => console.log(err));
     }
 
 });
+
 
 // Program start here
-db.connect((err) => {
-    if(err) {
-        console.log('Unable to connect to database');
-        process.exit(1);
-    } else {
-        app.listen(process.env.PORT || 8080, () => {
-            console.log('Connected to database, listening on port ' + APP_PORT);
-        })
-    }
-});
+MongoClient.connect(mongoUrl, mongoOptions)
+.then((client) => {
+    // update global db instance
+    db = client.db(dbname).collection(collection);
 
+    // start serving application
+    app.listen(process.env.PORT || 8080, () => {
+        console.log('Connected to database, listening on port ' + APP_PORT);
+    })
+})
+.catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
