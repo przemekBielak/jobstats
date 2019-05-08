d385116fc68167e54a68c155a874fc55c2fc7c35
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-05-08 21:50:13 +0200

    reformating of db functions in webparser

--- db/src/server.js
+++ db/src/server.js
@@ -3,9 +3,15 @@
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const MongoClient = require("mongodb").MongoClient;
-const db = require("./db");
 import jobParser from "./content";
 import { sleep } from "./common";
+
+const mongoUrl = "mongodb://localhost:27017/";
+const dbname = "jobs";
+const dbcollection = "jobs";
+const mongoOptions = { useNewUrlParser: true };
+let db = null;
+let dbclient = null;
 
 const url = "https://nofluffjobs.com/";
 const category = [
@@ -26,17 +32,16 @@ const category = [
 
 let jobLinks = [];
 
-// db related
-const collection = "jobs";
-
-const closeDB = async () => new Promise(resolve => db.getDbClient().close());
+const closeDB = async () => new Promise(resolve => dbclient.close());
 
 // Program start here
-db.connect(err => {
+MongoClient.connect(mongoUrl, mongoOptions, (err, client) => {
   if (err) {
     console.log("Unable to connect to database");
     process.exit(1);
   } else {
+    db = client.db(dbname);
+    dbclient = client;
     console.log("Connected to database");
   }
 });
@@ -66,50 +71,41 @@ db.connect(err => {
 
         // check if link exists in db
         const exists = await db
-          .getDB()
-          .collection(collection)
+          .collection(dbcollection)
           .countDocuments({ _id: jobLinks[i] });
         if (!exists) {
           // save parsed data to db
           let newDoc = await jobParser(jobLinks[i], category[iter]);
-          db.getDB()
-            .collection(collection)
-            .insertOne(newDoc);
+          db.collection(dbcollection).insertOne(newDoc);
           console.log("Saved " + jobLinks[i] + " to db");
 
           // add new must have requirements to requirementsMustHaveAll list in db
           const mustDoc = await db
-            .getDB()
-            .collection(collection)
+            .collection(dbcollection)
             .findOne({ _id: "requirementsMustHaveAll" });
           newDoc.requirementsMustHave.forEach(x => {
             if (!mustDoc.requirementsMustHaveAll.includes(x)) {
               mustDoc.requirementsMustHaveAll.push(x);
             }
           });
-          await db
-            .getDB()
-            .collection(collection)
-            .updateOne(
-              { _id: "requirementsMustHaveAll" },
-              {
-                $set: {
-                  requirementsMustHaveAll: mustDoc.requirementsMustHaveAll
-                }
+          await db.collection(dbcollection).updateOne(
+            { _id: "requirementsMustHaveAll" },
+            {
+              $set: {
+                requirementsMustHaveAll: mustDoc.requirementsMustHaveAll
               }
-            );
+            }
+          );
 
           // add new cities to citiesAll list in db
           const citiesDoc = await db
-            .getDB()
-            .collection(collection)
+            .collection(dbcollection)
             .findOne({ _id: "citiesAll" });
           if (!citiesDoc.citiesAll.includes(newDoc.companyLocationCity)) {
             citiesDoc.citiesAll.push(newDoc.companyLocationCity);
           }
           await db
-            .getDB()
-            .collection(collection)
+            .collection(dbcollection)
             .updateOne(
               { _id: "citiesAll" },
               { $set: { citiesAll: citiesDoc.citiesAll } }
