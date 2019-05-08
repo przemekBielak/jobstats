00731f102e64bd2ee2af4e42c0923e559472e9e3
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-05-08 20:25:00 +0200

    db styling

--- db/src/content.js
+++ db/src/content.js
@@ -2,252 +2,280 @@
 // TODO: Fully remote job option after salary view (country, city, when to start)
 // TODO: Add error checks for salary. Example: Unpaid
 
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
 function convertSalaryToNumber(salary) {
-    let val = 0;
-    
-    // get last character
-    if(salary.toLowerCase().slice(-1) == 'k') {
-        val = parseFloat(salary.slice(0, -1), 10) * 1000;
-    }else {
-        val = parseFloat(salary, 10);
-    }
+  let val = 0;
 
-    return val;
-};
+  // get last character
+  if (salary.toLowerCase().slice(-1) == "k") {
+    val = parseFloat(salary.slice(0, -1), 10) * 1000;
+  } else {
+    val = parseFloat(salary, 10);
+  }
 
+  return val;
+}
 
 var jobInfo = {
-    _id: '',
-    date: null,
-    category: '',
-    position: '',
-    salary: [],
-    typeOfContract: '',
-    seniorityLevel: [],
-    whenToStart: '',
-    companyName: '',
-    companySize: '',
-    companyLocationCity: '',
-    companyLocationCountry: '',
-    requirementsMustHave: [],
-    requirementsNices: [],
-    workMethodology: {},
-    os: {},
-    computer: '',
-    monitors: '',
-    specs: {},
-    perks: [],
-    benefits: [],
+  _id: "",
+  date: null,
+  category: "",
+  position: "",
+  salary: [],
+  typeOfContract: "",
+  seniorityLevel: [],
+  whenToStart: "",
+  companyName: "",
+  companySize: "",
+  companyLocationCity: "",
+  companyLocationCountry: "",
+  requirementsMustHave: [],
+  requirementsNices: [],
+  workMethodology: {},
+  os: {},
+  computer: "",
+  monitors: "",
+  specs: {},
+  perks: [],
+  benefits: []
 };
 
 // Returns parsed job offer website data as json object
 export default async (url, category) => {
+  jobInfo._id = url;
+  jobInfo.date = new Date();
+  jobInfo.category = category;
 
-    jobInfo._id = url;
-    jobInfo.date = new Date();
-    jobInfo.category = category;
+  console.log(`Started parsing ${url}`);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitFor(4000);
+  let content = await page.content();
 
-    console.log(`Started parsing ${url}`)
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitFor(4000);
-    let content = await page.content();
+  var $ = cheerio.load(content);
 
-    var $ = cheerio.load(content);
+  // console.log(content);
 
-    // console.log(content);
+  // check job position
+  jobInfo.position = $(".posting-header-description h1").text();
 
-    // check job position
-    jobInfo.position = $('.posting-header-description h1').text();
+  let info = [];
+  $(".posting-header-description dd").each(function(i, elem) {
+    // console.log($(this).text());
+    info[i] = $(this).text();
+  });
 
-    let info = []
-    $('.posting-header-description dd').each(function(i, elem) {
-        // console.log($(this).text());
-        info[i] = $(this).text();
-    });
+  // check basic info
+  jobInfo.companyName = info[0];
+  jobInfo.companySize = info[1];
+  const companyLocation = info[2].split(", ");
+  jobInfo.companyLocationCity = companyLocation[0];
+  let LocationCountrySplit = companyLocation[1].split(" ");
+  jobInfo.companyLocationCountry = LocationCountrySplit[0].trim();
+  jobInfo.whenToStart = info[3];
+  jobInfo.typeOfContract = info[4].trim();
+  jobInfo.seniorityLevel = info[5].split(", ");
 
-    // check basic info
-    jobInfo.companyName = info[0];
-    jobInfo.companySize = info[1];
-    const companyLocation = (info[2]).split(', ');
-    jobInfo.companyLocationCity = companyLocation[0];
-    let LocationCountrySplit = (companyLocation[1]).split(' ');
-    jobInfo.companyLocationCountry = LocationCountrySplit[0].trim();
-    jobInfo.whenToStart = info[3];
-    jobInfo.typeOfContract = (info[4]).trim();
-    jobInfo.seniorityLevel = (info[5]).split(', ');
+  // check salary info
+  let salaryMin = [];
+  let salaryMax = [];
+  let salaryCurrency = [];
+  $(".posting-main-info h4").each(function(i, item) {
+    let salary = $(this)
+      .text()
+      .split(" ");
 
-    // check salary info
-    let salaryMin = [];
-    let salaryMax = [];
-    let salaryCurrency = [];
-    $('.posting-main-info h4').each(function(i, item) {
-        let salary = $(this).text().split(' ');
-        
-        // Unpaid category
-        if(salary == 'Unpaid') {
-            salaryMin[i] = '0';
-            salaryMax[i] = '0';
-            salaryCurrency[i] = '';
-        }
-        // check if minus sign is in text. It signalizes salary range, not a fixed value.
-        else if(salary.indexOf('-') >= 0) {
-            salaryMin[i] = salary[0];
-            salaryMax[i] = salary[2];
-            salaryCurrency[i] = salary[3];
-        }
-        else {
-            salaryMin[i] = salary[0];
-            salaryMax[i] = salary[0];
-            salaryCurrency[i] = salary[1];
-        }
-    })
-    
-    let contractType = [];
-    let salaryRate = [];
-    $('.posting-main-info p').each(function(i, item) {
-        
-        // check contract type
-        let salaryInfoText = $(this).text();
-        if(salaryInfoText.toUpperCase().includes('B2B')) {
-            contractType[i] = 'B2B';
-        } 
-        else if(salaryInfoText.toUpperCase().includes('UOP')) {
-            contractType[i] = 'UoP';
-        }
-        else if(salaryInfoText.toUpperCase().includes('UZ')) {
-            contractType[i] = 'UZ';
-        }
-        else if(salaryInfoText.toUpperCase().includes('UOD')) {
-            contractType[i] = 'UoD';
-        }
-        else {
-            contractType[i] = '';
-        }
+    // Unpaid category
+    if (salary == "Unpaid") {
+      salaryMin[i] = "0";
+      salaryMax[i] = "0";
+      salaryCurrency[i] = "";
+    }
+    // check if minus sign is in text. It signalizes salary range, not a fixed value.
+    else if (salary.indexOf("-") >= 0) {
+      salaryMin[i] = salary[0];
+      salaryMax[i] = salary[2];
+      salaryCurrency[i] = salary[3];
+    } else {
+      salaryMin[i] = salary[0];
+      salaryMax[i] = salary[0];
+      salaryCurrency[i] = salary[1];
+    }
+  });
 
-        // check payment rate
-        if(salaryInfoText.toUpperCase().includes('MONTH')) {
-            salaryRate[i] = 'month';
-        }
-        else if(salaryInfoText.toUpperCase().includes('DAY')) {
-            salaryRate[i] = 'day';
-        }
-        else if(salaryInfoText.toUpperCase().includes('HOUR')) {
-            salaryRate[i] = 'hour';
-        }  
-        else if(salaryInfoText.toUpperCase().includes('YEAR')) {
-            salaryRate[i] = 'year';
-        }       
-        else {
-            salaryRate[i] = '';
-        }
-    })
-
-    jobInfo.salary = []
-    // save all salary related data in one array of objects
-    for(let i = 0; i < salaryMin.length; i++) {
-        jobInfo.salary[i] = {}
-        jobInfo.salary[i]['salaryMin'] = convertSalaryToNumber(salaryMin[i]);
-        jobInfo.salary[i]['salaryMax'] = convertSalaryToNumber(salaryMax[i]);
-        jobInfo.salary[i]['salaryCurrency'] = salaryCurrency[i];
-        jobInfo.salary[i]['contractType'] = contractType[i];
-        jobInfo.salary[i]['salaryRate'] = salaryRate[i];
+  let contractType = [];
+  let salaryRate = [];
+  $(".posting-main-info p").each(function(i, item) {
+    // check contract type
+    let salaryInfoText = $(this).text();
+    if (salaryInfoText.toUpperCase().includes("B2B")) {
+      contractType[i] = "B2B";
+    } else if (salaryInfoText.toUpperCase().includes("UOP")) {
+      contractType[i] = "UoP";
+    } else if (salaryInfoText.toUpperCase().includes("UZ")) {
+      contractType[i] = "UZ";
+    } else if (salaryInfoText.toUpperCase().includes("UOD")) {
+      contractType[i] = "UoD";
+    } else {
+      contractType[i] = "";
     }
 
-
-    // All requirements
-    $('.requirement.ng-binding.ng-scope').each(function(i, elem) {
-        jobInfo.requirementsMustHave[i] = $(this).text();
-    });
-
-    // Nices requirements
-    $("[ng-repeat='tech in formData.requirements.nices']").each(function(i, elem) {
-        // console.log(item);
-        jobInfo.requirementsNices[i] = $(this).text();
-    })
-
-    // Check if nices requirements are also in must have requirements. If yes, then delete.
-    for(let i = 0; i < jobInfo.requirementsNices.length; i++) {
-        if(jobInfo.requirementsMustHave.includes(jobInfo.requirementsNices[i])) {
-            let index = jobInfo.requirementsMustHave.indexOf(jobInfo.requirementsNices[i]);
-            jobInfo.requirementsMustHave.splice(index, 1);
-        }
+    // check payment rate
+    if (salaryInfoText.toUpperCase().includes("MONTH")) {
+      salaryRate[i] = "month";
+    } else if (salaryInfoText.toUpperCase().includes("DAY")) {
+      salaryRate[i] = "day";
+    } else if (salaryInfoText.toUpperCase().includes("HOUR")) {
+      salaryRate[i] = "hour";
+    } else if (salaryInfoText.toUpperCase().includes("YEAR")) {
+      salaryRate[i] = "year";
+    } else {
+      salaryRate[i] = "";
     }
+  });
 
-    // get all methodolody keys
-    var workMethodologyKey = [];
-    $("[ng-repeat='tool in tools'] .col-sm-6.p-label-row.ng-binding").each(function(i, elem) {
-        let key = $(this).text().replace(/\n/g, '');
-        workMethodologyKey[i] = key.replace(/\./g, '');
-    })
+  jobInfo.salary = [];
+  // save all salary related data in one array of objects
+  for (let i = 0; i < salaryMin.length; i++) {
+    jobInfo.salary[i] = {};
+    jobInfo.salary[i]["salaryMin"] = convertSalaryToNumber(salaryMin[i]);
+    jobInfo.salary[i]["salaryMax"] = convertSalaryToNumber(salaryMax[i]);
+    jobInfo.salary[i]["salaryCurrency"] = salaryCurrency[i];
+    jobInfo.salary[i]["contractType"] = contractType[i];
+    jobInfo.salary[i]["salaryRate"] = salaryRate[i];
+  }
 
-    // get all methodology vals
-    var workMethodologyVal = [];
-    $("[ng-repeat='tool in tools'] .col-sm-6.p-value-row dd").each(function(i, elem) {
-        workMethodologyVal[i] = $(this).text().split(',');
-    })
+  // All requirements
+  $(".requirement.ng-binding.ng-scope").each(function(i, elem) {
+    jobInfo.requirementsMustHave[i] = $(this).text();
+  });
 
-    // combine keys and vals as an object
-    workMethodologyKey.forEach((key, i) => jobInfo.workMethodology[key] = workMethodologyVal[i]);
+  // Nices requirements
+  $("[ng-repeat='tech in formData.requirements.nices']").each(function(
+    i,
+    elem
+  ) {
+    // console.log(item);
+    jobInfo.requirementsNices[i] = $(this).text();
+  });
 
-    // OS - mac
-    if ($("[ng-if='vm.isActiveSection('benefits.equipment')'] icon#apple.icon-blue.active").length) {
-        jobInfo.os['mac'] = true;
-    }else {
-        jobInfo.os['mac'] = false;
+  // Check if nices requirements are also in must have requirements. If yes, then delete.
+  for (let i = 0; i < jobInfo.requirementsNices.length; i++) {
+    if (jobInfo.requirementsMustHave.includes(jobInfo.requirementsNices[i])) {
+      let index = jobInfo.requirementsMustHave.indexOf(
+        jobInfo.requirementsNices[i]
+      );
+      jobInfo.requirementsMustHave.splice(index, 1);
     }
+  }
 
-    // OS - windows
-    if ($("[ng-if='vm.isActiveSection('benefits.equipment')'] icon#windows.icon-blue.active").length) {
-        jobInfo.os['windows'] = true;
-    }else {
-        jobInfo.os['windows'] = false;
+  // get all methodolody keys
+  var workMethodologyKey = [];
+  $("[ng-repeat='tool in tools'] .col-sm-6.p-label-row.ng-binding").each(
+    function(i, elem) {
+      let key = $(this)
+        .text()
+        .replace(/\n/g, "");
+      workMethodologyKey[i] = key.replace(/\./g, "");
     }
+  );
 
-    // OS - linux
-    if ($("[ng-if='vm.isActiveSection('benefits.equipment')'] icon#linux.icon-blue.active").length) {
-        jobInfo.os['linux'] = true;
-    }else {
-        jobInfo.os['linux'] = false;
+  // get all methodology vals
+  var workMethodologyVal = [];
+  $("[ng-repeat='tool in tools'] .col-sm-6.p-value-row dd").each(function(
+    i,
+    elem
+  ) {
+    workMethodologyVal[i] = $(this)
+      .text()
+      .split(",");
+  });
+
+  // combine keys and vals as an object
+  workMethodologyKey.forEach(
+    (key, i) => (jobInfo.workMethodology[key] = workMethodologyVal[i])
+  );
+
+  // OS - mac
+  if (
+    $(
+      "[ng-if='vm.isActiveSection('benefits.equipment')'] icon#apple.icon-blue.active"
+    ).length
+  ) {
+    jobInfo.os["mac"] = true;
+  } else {
+    jobInfo.os["mac"] = false;
+  }
+
+  // OS - windows
+  if (
+    $(
+      "[ng-if='vm.isActiveSection('benefits.equipment')'] icon#windows.icon-blue.active"
+    ).length
+  ) {
+    jobInfo.os["windows"] = true;
+  } else {
+    jobInfo.os["windows"] = false;
+  }
+
+  // OS - linux
+  if (
+    $(
+      "[ng-if='vm.isActiveSection('benefits.equipment')'] icon#linux.icon-blue.active"
+    ).length
+  ) {
+    jobInfo.os["linux"] = true;
+  } else {
+    jobInfo.os["linux"] = false;
+  }
+
+  // Get Computer
+  jobInfo.computer = $(
+    "[once-if='formData.benefits.equipment.computer !== '''] [tooltip-enable='formData.benefits.equipment.computer.length > 39']"
+  ).text();
+
+  // Get Monitors
+  jobInfo.monitors = $(
+    "[once-if='formData.benefits.equipment.monitors !== '''] [tooltip-enable='formData.benefits.equipment.monitors.length > 39']"
+  ).text();
+
+  // Get specs keys
+  var SpecsKeys = [];
+  $("[id='specs-block'] .col-sm-6.p-label-row").each(function(i, item) {
+    SpecsKeys[i] = $(this)
+      .text()
+      .replace(/\./g, "");
+  });
+
+  // Get specs vals
+  var SpecsVals = [];
+  $("[id='specs-block'] .col-sm-6.p-value-row").each(function(i, item) {
+    SpecsVals[i] = $(this)
+      .text()
+      .trim();
+  });
+
+  // Combine specs keys and vals
+  SpecsKeys.forEach((key, i) => (jobInfo.specs[key] = SpecsVals[i]));
+
+  // Perks
+  $(
+    ".panel.border-top.this-and-that.ng-isolate-scope.border-top4 dd.ng-binding"
+  ).each(function(i, item) {
+    jobInfo.perks[i] = $(this).text();
+  });
+
+  // Benefits
+  $("[ng-if='vm.isActiveSection('benefits.benefits')'] dd.ng-binding").each(
+    function(i, item) {
+      jobInfo.benefits[i] = $(this).text();
     }
-    
-    // Get Computer 
-    jobInfo.computer = $("[once-if='formData.benefits.equipment.computer !== '''] [tooltip-enable='formData.benefits.equipment.computer.length > 39']").text();
+  );
 
-    // Get Monitors 
-    jobInfo.monitors = $("[once-if='formData.benefits.equipment.monitors !== '''] [tooltip-enable='formData.benefits.equipment.monitors.length > 39']").text();
+  await browser.close();
 
-    // Get specs keys
-    var SpecsKeys = [];
-    $("[id='specs-block'] .col-sm-6.p-label-row").each(function(i, item) {
-        SpecsKeys[i] = $(this).text().replace(/\./g, '');
-    });
-
-    // Get specs vals
-    var SpecsVals = [];
-    $("[id='specs-block'] .col-sm-6.p-value-row").each(function(i, item) {
-        SpecsVals[i] = ($(this).text()).trim();
-    });
-
-    // Combine specs keys and vals
-    SpecsKeys.forEach((key, i) => jobInfo.specs[key] = SpecsVals[i]);
-
-    // Perks
-    $(".panel.border-top.this-and-that.ng-isolate-scope.border-top4 dd.ng-binding").each(function(i, item) {
-        jobInfo.perks[i] = $(this).text();
-    })
-
-    // Benefits
-    $("[ng-if='vm.isActiveSection('benefits.benefits')'] dd.ng-binding").each(function(i, item) {
-        jobInfo.benefits[i] = $(this).text();
-    })
-
-
-    await browser.close();
-
-    return jobInfo;
-}
+  return jobInfo;
+};
--- db/src/server.js
+++ db/src/server.js
@@ -1,28 +1,27 @@
 // TODO: check other categories like frontend, devops, etc.
 
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
-const MongoClient = require('mongodb').MongoClient;
-const db = require('./db');
-import jobParser from './content';
-import { sleep } from './common'
-
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const MongoClient = require("mongodb").MongoClient;
+const db = require("./db");
+import jobParser from "./content";
+import { sleep } from "./common";
 
 const url = "https://nofluffjobs.com/";
 const category = [
-    'backend',
-    'fullstack',
-    'mobile',
-    'frontend',
-    'testing',
-    'devops',
-    'hr',
-    'trainee',
-    'ux',
-    'support',
-    'project-manager',
-    'business-analyst',
-    'other'
+  "backend",
+  "fullstack",
+  "mobile",
+  "frontend",
+  "testing",
+  "devops",
+  "hr",
+  "trainee",
+  "ux",
+  "support",
+  "project-manager",
+  "business-analyst",
+  "other"
 ];
 
 var jobLinks = [];
@@ -31,95 +30,113 @@ var jobLinks = [];
 const collection = "jobs";
 
 async function closeDB() {
-    return new Promise(resolve => db.getDbClient().close());
+  return new Promise(resolve => db.getDbClient().close());
 }
 
-
-
-
 // Program start here
-db.connect((err) => {
-    if(err) {
-        console.log('Unable to connect to database');
-        process.exit(1);
-    } else {
-        console.log('Connected to database');
-    }
+db.connect(err => {
+  if (err) {
+    console.log("Unable to connect to database");
+    process.exit(1);
+  } else {
+    console.log("Connected to database");
+  }
 });
 
 (async () => {
-    for(let iter = 0; iter < category.length; iter++) {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url + category[iter]);
-        await page.waitFor(3000);
-        let content = await page.content();
-        
-        var $ = cheerio.load(content);
-    
-        // get all links
-        let links = $('.col-sm-12 .list-item ');
-        for(var i = 0; i < links.length; i++) {
-            jobLinks.push('https://nofluffjobs.com' + links[i].attribs.href);
-        }
-    
-        console.log(`--> Got ${jobLinks.length} links from ${category[iter]}`);
-        await browser.close();
-    
-        // parse 30 newest links, or less when not possible
-        // let parseLimit = 100;
-        // if(jobLinks.length < parseLimit) {
-        //     parseLimit = jobLinks.length;
-        // }
-        // check if parsed any links
-        if(jobLinks.length != 0) {
-            for(let i = 0; i < jobLinks.length; i++) {
-                const timeCounter = Math.floor((Math.random() * 30000) + 2000);
-        
-                // check if link exists in db
-                var exists = await db.getDB().collection(collection).countDocuments({"_id":jobLinks[i]});
-                if(!exists) {
-                    // save parsed data to db
-                    let newDoc = await jobParser(jobLinks[i], category[iter]);
-                    db.getDB().collection(collection).insertOne(newDoc);
-                    console.log("Saved " + jobLinks[i] + " to db");
+  for (let iter = 0; iter < category.length; iter++) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url + category[iter]);
+    await page.waitFor(3000);
+    let content = await page.content();
 
-                    // add new must have requirements to requirementsMustHaveAll list in db
-                    var mustDoc = await db.getDB().collection(collection).findOne({"_id":"requirementsMustHaveAll"});
-                    newDoc.requirementsMustHave.forEach(x => {
-                        if(!mustDoc.requirementsMustHaveAll.includes(x)) {
-                            mustDoc.requirementsMustHaveAll.push(x);
-                        }
-                    });
-                    await db.getDB().collection(collection).updateOne({"_id":"requirementsMustHaveAll"}, {$set: {requirementsMustHaveAll: mustDoc.requirementsMustHaveAll}});
+    var $ = cheerio.load(content);
 
-                    // add new cities to citiesAll list in db
-                    var citiesDoc = await db.getDB().collection(collection).findOne({"_id":"citiesAll"});
-                    if(!citiesDoc.citiesAll.includes(newDoc.companyLocationCity)) {
-                        citiesDoc.citiesAll.push(newDoc.companyLocationCity);
-                    }
-                    await db.getDB().collection(collection).updateOne({"_id":"citiesAll"}, {$set: {citiesAll: citiesDoc.citiesAll}});
-
-    
-    
-                    // wait before next parsing
-                    console.log(`Waiting for ${timeCounter/1000} seconds`)
-                    await sleep(timeCounter);
-          
-                } else {
-                    console.log(jobLinks[i] + " already exists in db")
-                }
-            }
-        } else {
-            console.log("Error while getting links");
-        }
-
-        // Clear links array for another category
-        jobLinks = [];
-        
+    // get all links
+    let links = $(".col-sm-12 .list-item ");
+    for (var i = 0; i < links.length; i++) {
+      jobLinks.push("https://nofluffjobs.com" + links[i].attribs.href);
     }
 
-    await closeDB();
+    console.log(`--> Got ${jobLinks.length} links from ${category[iter]}`);
+    await browser.close();
 
+    // parse 30 newest links, or less when not possible
+    // let parseLimit = 100;
+    // if(jobLinks.length < parseLimit) {
+    //     parseLimit = jobLinks.length;
+    // }
+    // check if parsed any links
+    if (jobLinks.length != 0) {
+      for (let i = 0; i < jobLinks.length; i++) {
+        const timeCounter = Math.floor(Math.random() * 30000 + 2000);
+
+        // check if link exists in db
+        var exists = await db
+          .getDB()
+          .collection(collection)
+          .countDocuments({ _id: jobLinks[i] });
+        if (!exists) {
+          // save parsed data to db
+          let newDoc = await jobParser(jobLinks[i], category[iter]);
+          db.getDB()
+            .collection(collection)
+            .insertOne(newDoc);
+          console.log("Saved " + jobLinks[i] + " to db");
+
+          // add new must have requirements to requirementsMustHaveAll list in db
+          var mustDoc = await db
+            .getDB()
+            .collection(collection)
+            .findOne({ _id: "requirementsMustHaveAll" });
+          newDoc.requirementsMustHave.forEach(x => {
+            if (!mustDoc.requirementsMustHaveAll.includes(x)) {
+              mustDoc.requirementsMustHaveAll.push(x);
+            }
+          });
+          await db
+            .getDB()
+            .collection(collection)
+            .updateOne(
+              { _id: "requirementsMustHaveAll" },
+              {
+                $set: {
+                  requirementsMustHaveAll: mustDoc.requirementsMustHaveAll
+                }
+              }
+            );
+
+          // add new cities to citiesAll list in db
+          var citiesDoc = await db
+            .getDB()
+            .collection(collection)
+            .findOne({ _id: "citiesAll" });
+          if (!citiesDoc.citiesAll.includes(newDoc.companyLocationCity)) {
+            citiesDoc.citiesAll.push(newDoc.companyLocationCity);
+          }
+          await db
+            .getDB()
+            .collection(collection)
+            .updateOne(
+              { _id: "citiesAll" },
+              { $set: { citiesAll: citiesDoc.citiesAll } }
+            );
+
+          // wait before next parsing
+          console.log(`Waiting for ${timeCounter / 1000} seconds`);
+          await sleep(timeCounter);
+        } else {
+          console.log(jobLinks[i] + " already exists in db");
+        }
+      }
+    } else {
+      console.log("Error while getting links");
+    }
+
+    // Clear links array for another category
+    jobLinks = [];
+  }
+
+  await closeDB();
 })();
-
